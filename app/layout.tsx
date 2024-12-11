d75f34df418bd59bd2be7f583084571b4c8ab74f
Weri Oliveira <werioliveira@hotmail.com>
2024-12-11 20:12:55 -0300

    add analitics

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
+        <script async src="https://analytic.werioliveira.shop/tracker.js" data-ackee-server="https://analytic.werioliveira.shop" data-ackee-domain-id="31527166-b1bb-44e7-8d4e-ece3e4d17f9c"></script>
       </body>
     </html>
   )
